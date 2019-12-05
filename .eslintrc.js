feac79b62f350231bf59aa5f1b33e9087f59a7ed
Cilvia333 <cilvia333x@gmail.com>
2019-12-05 19:37:35 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,69 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		node: true,
+		es6: true
+	},
+	parser: 'vue-eslint-parser',
+	parserOptions: {
+		parser: '@typescript-eslint/parser',
+		ecmaFeatures: { legacyDecorators: true }
+	},
+	extends: [
+		'@nuxtjs/eslint-config-typescript',
+		'plugin:nuxt/recommended',
+		'airbnb-base',
+		'plugin:eslint-comments/recommended',
+		'plugin:prettier/recommended',
+		'prettier/vue',
+		'prettier/@typescript-eslint'
+	],
+	plugins: ['vue', 'prettier', '@typescript-eslint', 'json'],
+	settings: {
+		'import/core-modules': ['vue', 'vuex','axios'],
+		'import/resolver': {
+			nuxt: {
+				nuxtSrcDir: 'src',
+				extensions: ['.js', '.ts', '.json', '.vue']
+			}
+		}
+	},
+	// add your custom rules here
+	rules: {
+		//'class-methods-use-this': 0,
+		'prettier/prettier': 'error',
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': 'warn',
+		'import/no-unresolved': 'warn',
+		'unicorn/number-literal-case': 'off',
+		'import/no-dynamic-require': 'off',
+		'global-require': 'off',
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'vue/component-name-in-template-casing': [
+			'error',
+			'PascalCase',
+			{
+				ignores: [
+					'nuxt',
+					'nuxt-link',
+					'n-link',
+					'nuxt-child',
+          'no-ssr',
+          'client-only',
+					'svg-icon'
+				]
+			}
+		]
+	},
+	overrides: [
+		{
+			files: ['*.d.ts'],
+			rules: {
+				'import/no-extraneous-dependencies': 0
+			}
+		}
+	]
+}
+
---
+++ nuxt.config.ts
@@ -0,0 +1,88 @@
+import { Configuration } from '@nuxt/types'
+
+const config: Configuration = {
+  mode: 'universal',
+  srcDir: './src',
+  loading: false,
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: 'Virtual Idol Festival -公式サイト-',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: '今を煌めく新しいアイドルの形、それがVアイドル！今ここに界隈を盛り上げるVアイドルたちが一堂に会し、宴がはじまる。'
+      },
+      { hid: 'keywords',name: 'keywords', content: 'Vtuber, バーチャルアイドル'},
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Virtual Idol Festival' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://vdolfes.com' },
+      { hid: 'og:title', property: 'og:title', content: 'Virtual Idol Festival' },
+      { hid: 'og:description', property: 'og:description', content: 'バーチャルアイドルたちの祭典、それがバーチャルアイドルフェスティバル！' },
+      { hid: 'og:image', property: 'og:image', content: "/ogp.png"},
+      { hid: "twitter:card", property: "twitter:card", content: "summary" }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  
+  css: ['ress/ress.css'],
+
+  plugins: [],
+
+  buildModules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/stylelint-module',
+    '@nuxt/typescript-build'
+  ],
+
+  modules: [
+    '@bazzite/nuxt-optimized-images',
+    '@nuxtjs/style-resources',
+    '@nuxtjs/svg-sprite',
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    '@nuxtjs/dotenv',
+    ['vue-scrollto/nuxt', { 
+      duration: 300 
+    }],
+  ],
+
+  axios: {},
+  build: {
+    cache: true,
+		postcss: {
+			plugins: {
+				'postcss-short': {}
+			}
+		},
+    extend(config, ctx) {
+      if (!config.output) return
+      if (!config.module) return
+
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(ts|js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+          options : {
+            fix : true
+          }
+        })
+      }
+
+      if(!ctx.isDev) {
+        config.output.publicPath = '_nuxt/'
+     }
+    }
+  },
+  generate: {
+    fallback: true,
+  }
+}
+
+export default config
